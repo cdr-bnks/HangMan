679e410adc062ed6ed7978c689298d27653a5b6e
cdr-bnks <90341829+cdr-bnks@users.noreply.github.com>
2021-10-08 17:45:30 -0400

    Update Program.cs

--- HangManConsole/Program.cs
+++ HangManConsole/Program.cs
@@ -10,6 +10,17 @@ namespace HangManConsole
     {
         static void Main(string[] args)
         {
+            // 1.) Create Hangman [Main Menu]
+            // 2.) User friendly and can interact with the [Main Menu] business logic
+            // 3.) Keep an up to date collection
+            
+            // 1.0_(Main menu) conditionals will be: Booleans: While Loops, If else statements. Switch Cases: following CRUD process. Also a location where the user can rate the game. we will refactor in new content like levels & players - if we want to add those- using fields and constructs after the game is completed. 
+            
+            
+            //1.1_(Main menu) Simple step by step process of how-to-play. Applying the Console.WriteLine operator. *This will most likely be the last thing we get to.
+            
+            //Creating the Game
+
         }
     }
 }
